bf94f2d85f1c1202ba2a8b44a452d2a07fd109e0
Yosmar B <imyosbp@gmail.com>
2022-12-11 15:10:28 -0400

    Change all to spanish and fix some spaces and hovers.

--- js/main.js
+++ js/main.js
@@ -29,7 +29,6 @@ const linkAction = () => {
 };
 navLink.forEach((n) => n.addEventListener("click", linkAction));
 
-
 /*=============== EMAIL JS ===============*/
 const contactForm = document.getElementById("contact-form"),
   contactName = document.getElementById("contact-name"),
@@ -41,7 +40,11 @@ const sendEmail = (e) => {
   e.preventDefault();
 
   // Verifica si hay algun valor
-  if ( contactName.value === "" || contactEmail.value === "" || contactProject.value === "") {
+  if (
+    contactName.value === "" ||
+    contactEmail.value === "" ||
+    contactProject.value === ""
+  ) {
     // Añade o elimina color
     contactMessage.classList.remove("color-blue");
     contactMessage.classList.add("color-red");
@@ -50,21 +53,28 @@ const sendEmail = (e) => {
     contactMessage.textContent = "Llena todos los campos! 📩";
   } else {
     // serviceID - templateID - #form - publicKey
-    emailjs.sendForm('service_rhnl35v', 'template_xeeb64v', '#contact-form', 'BgHsx6ch0x_KIvga6').then(
-      () => {
-        // Enseña mensaje exitoso y añade color
-        contactMessage.classList.add("color-blue");
-        contactMessage.textContent = "Mensaje enviado ✅";
+    emailjs
+      .sendForm(
+        "service_rhnl35v",
+        "template_xeeb64v",
+        "#contact-form",
+        "BgHsx6ch0x_KIvga6"
+      )
+      .then(
+        () => {
+          // Enseña mensaje exitoso y añade color
+          contactMessage.classList.add("color-blue");
+          contactMessage.textContent = "Mensaje enviado ✅";
 
-        // Elimina el mensaje despues de 5 segundos
-        setTimeout(() => {
-          contactMessage.textContent = "";
-        }, 5000);
-      },
-      (error) => {
-        alert("OOPS! ALGO HA FALLADO!...", error);
-      }
-    );
+          // Elimina el mensaje despues de 5 segundos
+          setTimeout(() => {
+            contactMessage.textContent = "";
+          }, 5000);
+        },
+        (error) => {
+          alert("OOPS! ALGO HA FALLADO!...", error);
+        }
+      );
 
     // Limpia todos los valores
     contactName.value = "";
@@ -73,3 +83,46 @@ const sendEmail = (e) => {
   }
 };
 contactForm.addEventListener("submit", sendEmail);
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll("section[id]");
+
+const scrollActive = () => {
+  const scrollY = window.pageYOffset;
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight,
+      sectionTop = current.offsetTop - 58,
+      sectionId = current.getAttribute("id"),
+      sectionsClass = document.querySelector(
+        ".nav__menu a[href*=" + sectionId + "]"
+      );
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      sectionsClass.classList.add("active-link");
+    } else {
+      sectionsClass.classList.remove("active-link");
+    }
+  });
+};
+window.addEventListener("scroll", scrollActive);
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+  origin: "top",
+  distance: "24px",
+  duration: 2500,
+  delay: 400,
+  // reset: true // Animations repeat
+});
+
+sr.reveal(
+  `.home__data, .projects__container, .footer__container`
+);
+sr.reveal(`.home__info div`, { delay: 600, origin: "bottom", interval: 100 });
+sr.reveal(`.skills__content:nth-child(1), .contact__content:nth-child(1)`, {
+  origin: "left",
+});
+sr.reveal(`.skills__content:nth-child(2), .contact__content:nth-child(2)`, {
+  origin: "right",
+});
